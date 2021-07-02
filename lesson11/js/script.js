6a470e1f68c5027e6fc1b6af649a2e4cfda6b115
laurenmackhicks <laurenhouston@live.com>
2021-07-02 13:10:41 +0900

    11 js

---
+++ lesson11/js/script.js
@@ -0,0 +1,100 @@
+//preston, fish haven, soda springs
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.menu')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 468) mainnav.classList.remove('responsive')};
+
+const thedate = new Date();
+let year = thedate.getFullYear();
+document.getElementById("currentYear").textContent = year;
+
+const lastModDate = new Date(document.lastModified);
+let strLMD = + lastModDate.getMonth()+1 + "/" + lastModDate.getDate() + "/" 
+           + lastModDate.getFullYear() + " " + lastModDate.getHours() + ":" 
+           + lastModDate.getMinutes() + ":" + lastModDate.getSeconds();
+document.getElementById("lastUpdate").textContent = strLMD;
+
+if (thedate.getDay() == 5) {
+    document.querySelector('.announcement').style.display = 'block';
+}
+
+var closebtns = document.getElementsByClassName("close");
+var i;
+
+for (i = 0; i < closebtns.length; i++) {
+  closebtns[i].addEventListener("click", function() {
+    this.parentElement.style.display = 'none';
+  });
+}
+//gallery
+const imagesToDisplay=document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+const imgOptions = {
+    threshold:1,
+    rootMargin: "0px 0px 50px 0px"
+}; 
+
+if('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver(items => {
+        items.forEach(item => {
+            if (item.isIntersecting){
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    imagesToDisplay.forEach((img) => {
+        imgObserver.observe(img);
+    });
+}
+else {
+    imagesToDisplay.forEach((img) => {
+        loadImages(img);
+    });
+}
+//lazy
+const imagesToLoad=document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+const imgOptions = {
+    threshold:1,
+    rootMargin: "0px 0px 50px 0px"
+}; 
+
+if('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver(items => {
+        items.forEach(item => {
+            if (item.isIntersecting){
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+}
+else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
+//
