385ee81e837505e5bd39c7bf51636b0053782b97
laurenmackhicks <laurenhouston@live.com>
2021-07-02 12:12:42 +0900

    new repository

---
+++ lesson7/Java/gal.js
@@ -0,0 +1,33 @@
+const imagesToDisplay=document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+const imgOptions = {
+    threshold:1,
+    rootMargin: "0px 0px 50px 0px"
+}; 
+
+if('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver(items => {
+        items.forEach(item => {
+            if (item.isIntersecting){
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    imagesToDisplay.forEach((img) => {
+        imgObserver.observe(img);
+    });
+}
+else {
+    imagesToDisplay.forEach((img) => {
+        loadImages(img);
+    });
+}
