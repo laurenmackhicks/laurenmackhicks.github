52688a8da324e9053f1ae2870da3201304588d31
laurenmackhicks <laurenhouston@live.com>
2021-07-22 14:28:54 +0900

    directors

--- herrimanchamber/js/directors.js
+++ herrimanchamber/js/directors.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const requestURL = 'https://laurenmackhicks.github.io/laurenmackhicks.github/herrimanchamber/directors.json';
 
 fetch(requestURL)
   .then(function (response) {
@@ -13,18 +13,21 @@ fetch(requestURL)
         let h2 = document.createElement('h2');
         let p1 = document.createElement('p');
         let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
         let image = document.createElement('img');
 
-        h2.textContent = `${directors[i].name} ${prophets[i].lastname}`;
-        p1.textContent = directors[i].birthdate; 
-        p2.innerHTML = `Title: ${directors[i].title}`; 
+        h2.textContent = `${directors[i].name}`;
+        p1.textContent = directors[i].title; 
+        p2.innerHTML = `${directors[i].office}`;
+        p3.innerHTML = `${directors[i].email}`; 
         image.setAttribute('src', directors[i].imageurl);
-        image.setAttribute('alt', `${directors[i].name} ${directors[i].lastname}!`)
+        image.setAttribute('alt', `${directors[i].name}`)
         image.style.boxShadow = '0 0 30px #999'
 
         card.appendChild(h2);
         card.appendChild(p1);
         card.appendChild(p2);
+        card.appendChild(p3);
         card.appendChild(image);
 
         document.querySelector('div.directors').appendChild(card);
